ee620d04edd0f36cd0b73600ec487e8af9f6ca58
Bakhoo <boxo2707@mail.ru>
2018-11-19 23:27:57 +0500

    [R]

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,27 @@
-function myFunction(x){
-    x.classList.toggle("change");
+function myFunction(x) {
+  x.classList.toggle("change");
 }
 
-function myFunk(x){
-    x.classList.toggle("tabblock");
+function myFunk(x) {
+  x.classList.toggle("tabblock");
 }
 
-function openPage(pageName,elmnt,color) {
-    var i,tabcontent,tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for(i= 0; i < tabcontent.length; i++){
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++){
-        tablinks[i].style.backgroundColor = "";
-    }
-    document.getElementById(pageName).style.display = "block";
-    elmnt.style.backgroundColor = color;
+function openPage(pageName, elmnt, color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
 }
 document.getElementById("defaultOpen").click();
 
-function mix(x,pageName,elmnt,color) {
-    myFunk(x);
-    openPage(pageName,elmnt,color);
+function mix(x, pageName, elmnt, color) {
+  myFunk(x);
+  openPage(pageName, elmnt, color);
 }
-
-
